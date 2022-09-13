6a5215f145dd3aafff9c7b1cc5ddc2663a4741da
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-09-13 12:08:27 +0000

    feat: add function to get the config

--- src/cargo/integration.ts
+++ src/cargo/integration.ts
@@ -2,6 +2,7 @@ import { setup } from "../mod.ts";
 import { getStyleTag, virtualSheet } from "../sheets/mod.ts";
 
 export const vsheet = virtualSheet();
+let twindConfig: any = {};
 
 export const TwindIntegration = {
   getStyles(): string {
@@ -10,9 +11,14 @@ export const TwindIntegration = {
     vsheet.reset();
     return styleTag;
   },
+  getConfig(): any {
+    const { ...config } = twindConfig;
+    return config;
+  },
 };
 
 export function setupTwind(config: any): () => void {
+  twindConfig = config;
   return () => {
     setup({ sheet: vsheet, ...config });
   };
