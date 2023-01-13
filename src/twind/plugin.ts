5940014eb1690c1f6257d6a6497c8b58b580719a
Daniel Steuri <daniel.steuri@flowingones.ch>
2023-01-13 13:31:08 +0000

    refactor: make hash configurable

--- src/twind/plugin.ts
+++ src/twind/plugin.ts
@@ -2,7 +2,11 @@ import { inline, install, TwindConfig } from "@twind/core";
 import { AfterRenderTaskContext } from "parcel/cargo/plugin.ts";
 
 export function TwindPlugin<T extends TwindConfig>(options: T) {
-  install(options, false);
+  // TODO: only default to false if islands are available
+  if (typeof options.hash === "undefined") {
+    options.hash = false;
+  }
+  install(options);
 
   return {
     name: "Twind Plugin",
