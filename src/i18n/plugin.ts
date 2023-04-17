29d56671d1022820db43d043a6e49710f2cd07ed
Daniel Steuri <daniel.steuri@flowingones.ch>
2023-04-17 22:59:43 +0200

    feat: redirect root to default language

--- src/i18n/plugin.ts
+++ src/i18n/plugin.ts
@@ -1,16 +1,29 @@
 import { Plugin, PluginDefintions } from "parcel/cargo/plugin.ts";
 import { BUILD_ID } from "parcel/cargo/constants.ts";
-import { I18nConfig, reset, set, setup } from "./i18n.ts";
+import {
+  extractLang,
+  getActiveLang,
+  getLanguages,
+  I18nConfig,
+  reset,
+  set,
+  setup,
+} from "./i18n.ts";
+import { Get, NotFoundException } from "cargo/http/mod.ts";
 
-export function I18nPlugin({
-  languages,
-  defaultLanguage,
-  pattern,
-}: I18nConfig): Plugin {
-  setup({ defaultLanguage, languages, pattern });
+export function I18nPlugin(config: I18nConfig): Plugin {
+  setup(config);
   return {
     name: "I18nPlugin",
     plugin(): PluginDefintions {
+      Get("/", ({ request }) => {
+        return Response.redirect(
+          new URL(
+            `/${config.defaultLanguage || "en"}`,
+            new URL(request.url).origin,
+          ),
+        );
+      });
       return {
         entryPoints: {
           plugin_i18n: new URL("./i18n.ts", import.meta.url).href,
@@ -18,20 +31,30 @@ export function I18nPlugin({
         scripts: [
           `<script type="module">import { setup } from "/_parcel/${BUILD_ID}/plugin_i18n.js"; setup(${
             JSON.stringify(
-              { defaultLanguage, languages, pattern },
+              config,
             )
           })</script>`,
         ],
         tasks: {
           beforeRender: [
             (ctx) => {
+              if (extractLang(new URL(ctx.request.url).href) === null) {
+                throw new NotFoundException();
+              }
+
               set(ctx.request);
+
+              if (!getLanguages().includes(getActiveLang())) {
+                reset();
+                throw new NotFoundException();
+              }
+              return ctx;
             },
           ],
           afterRender: [
             (ctx) => {
               reset();
-              return ctx.pageHtml;
+              return ctx;
             },
           ],
         },
