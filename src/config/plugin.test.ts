538c27ba081609aa7b55c7e143d492a871178110
Daniel Steuri <daniel.steuri@flowingones.ch>
2023-04-30 11:59:52 +0200

    feat: add config service to plugins

---
+++ src/config/plugin.test.ts
@@ -0,0 +1,39 @@
+import { get } from "assemble/mod.ts";
+import { assertEquals } from "std/testing/asserts.ts";
+import { Label } from "label/label.ts";
+import { ConfigPlugin } from "./plugin.ts";
+import { BUILD_ID } from "parcel/cargo/constants.ts";
+
+const labels = {
+  name: "Anakin",
+  "name!": "Luke",
+};
+
+const Plugin = ConfigPlugin({
+  labels,
+});
+
+const plugin = Plugin.plugin();
+
+Deno.test(ConfigPlugin.name, async (t) => {
+  await t.step('should have name "ConfigPlugin"', () => {
+    assertEquals(Plugin.name, "ConfigPlugin");
+  });
+
+  await t.step("should have all expected props", () => {
+    assertEquals(plugin.entryPoints, {
+      "plugin_config": new URL("main.ts", import.meta.url).href,
+    });
+    assertEquals(plugin.scripts, [
+      `<script type="module">import { config } from "/_parcel/${BUILD_ID}/plugin_config.js";config({"name!":"Luke"})</script>`,
+    ]);
+  });
+
+  await t.step("should have access to label in Cargo Assemble", () => {
+    assertEquals(
+      get<Label<typeof labels>>("config").get("name!"),
+      "Luke",
+    );
+    assertEquals(get<Label<typeof labels>>(Label).get("name"), "Anakin");
+  });
+});
---
+++ src/config/plugin.ts
@@ -0,0 +1,54 @@
+import { Label, Labels, LabelsOptions } from "label/label.ts";
+import { assemble, get } from "assemble/assemble.ts";
+import { PluginDefintions } from "parcel/cargo/plugin.ts";
+import { BUILD_ID } from "parcel/cargo/constants.ts";
+
+export function ConfigPlugin<T extends Labels>(options: LabelsOptions<T>) {
+  // Setup Label for the server
+  assemble({
+    token: "labels",
+    value: options,
+  });
+  assemble({ class: Label, dependencies: ["labels"] });
+  assemble({ token: "config", value: get(Label) });
+
+  const config = <Label<T>> get("config");
+
+  return {
+    name: "ConfigPlugin",
+    plugin(): PluginDefintions {
+      return {
+        entryPoints: {
+          plugin_config: new URL("./main.ts", import.meta.url).href,
+        },
+        scripts: [
+          `<script type="module">import { config } from "/_parcel/${BUILD_ID}/plugin_config.js";config(${
+            JSON.stringify(filterAllowedInBrowser(config.getAll()))
+          })</script>`,
+        ],
+      };
+    },
+  };
+}
+
+/**
+ * Function to filter only explicitly allowed labels for the browser.
+ * Only labels which are appended with a "!" are used in the browser.
+ */
+function filterAllowedInBrowser(labels: Labels) {
+  const allowedLabels: Labels = {};
+  for (const key in labels) {
+    const chars = [...key];
+    if (chars[chars.length - 1] === "!") {
+      if (
+        typeof labels[key] === "string" || typeof labels[key] === "number" ||
+        typeof labels[key] === "boolean"
+      ) {
+        allowedLabels[key] = labels[key];
+        continue;
+      }
+      allowedLabels[key] = filterAllowedInBrowser(<Labels> labels[key]);
+    }
+  }
+  return allowedLabels;
+}
